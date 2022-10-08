8c5bff53d3b399f834149f829545a755feaa9ea8
Mohammad Safia <mohammad.safia@meccasoft.com>
2022-10-09 02:26:07 +0300

    create-navbar

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,21 +1,28 @@
-import axios from 'axios'
-import {useState, useEffect} from 'react';
+import './styles.css';
+
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import { Activity } from 'types';
+import NavBar from "app/layout/NavBar";
+import { Container, List } from "semantic-ui-react";
 
 function App() {
-    const [activities, setActivities] = useState<Activity[]>([]);
-    useEffect(() => {
-        axios.get<Activity[]>('http://localhost:5000/api/activities').then(response => {
-            setActivities(response.data)
-        });
-    }, [])
-    return (
-        <div>
-            <ul>
-                {activities.map((activity) => <li key={activity.id}>{activity.title}</li>)}
-            </ul>
-        </div>
-    );
+  const [activities, setActivities] = useState<Activity[]>([]);
+  useEffect(() => {
+    axios.get<Activity[]>('http://localhost:5000/api/activities').then(response => {
+      setActivities(response.data);
+    });
+  }, []);
+  return (
+    <>
+      <NavBar/>
+      <Container style={{ marginTop: '7em' }}>
+        <List>
+          {activities.map((activity) => <List.Item key={activity.id}>{activity.title}</List.Item>)}
+        </List>
+      </Container>
+    </>
+  );
 }
 
 export default App;
