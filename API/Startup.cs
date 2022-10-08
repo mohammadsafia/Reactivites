77526e524a5360e03a736f92a7daafe97b525874
Mohammad Safia <mohammad.safia@meccasoft.com>
2022-10-08 21:47:59 +0300

    install axios, fix cors policy

--- API/Startup.cs
+++ API/Startup.cs
@@ -24,12 +24,11 @@ namespace API
         {
             _config = config;
         }
-        
+
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
@@ -38,6 +37,14 @@ namespace API
             services.AddDbContext<DataContext>(opt =>
             {
                 opt.UseSqlite(_config.GetConnectionString("DefaultConnection"));
+            });
+            services.AddCors(opt =>
+            {
+                opt.AddPolicy("CorsPolicy",
+                    policy =>
+                    {
+                        policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000");
+                    });
             });
         }
 
@@ -51,16 +58,17 @@ namespace API
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebAPIv5 v1"));
             }
 
+           
+
             // app.UseHttpsRedirection();
 
             app.UseRouting();
+            
+            app.UseCors("CorsPolicy");
 
             app.UseAuthorization();
 
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-            });
+            app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
         }
     }
 }
--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,26 +1,25 @@
-import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios'
+import {useState, useEffect} from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [activities, setActivities] = useState([]);
+    useEffect(() => {
+        axios.get('http://localhost:5000/api/activities').then(response => {
+            setActivities(response.data)
+        });
+    }, [])
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo"/>
+                <ul>
+                    {activities.map((activity:any)=> <li key={activity.id}>{activity.title}</li> )}
+                </ul>
+            </header>
+        </div>
+    );
 }
 
 export default App;
