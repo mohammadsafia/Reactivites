2c153de9cbda86fc60ee59ed2bf3a89e3206b3f1
Mohammad Safia <mohammad.safia@meccasoft.com>
2022-10-14 07:38:25 +0300

    Security header added

--- API/Startup.cs
+++ API/Startup.cs
@@ -26,12 +26,33 @@ public class Startup
     {
         app.UseMiddleware<ExceptionMiddleware>();
 
+        app.UseXContentTypeOptions();
+        app.UseReferrerPolicy(opt => opt.NoReferrer());
+        app.UseXXssProtection(opt => opt.EnabledWithBlockMode());
+        app.UseXfo(opt => opt.Deny());
+        app.UseCspReportOnly(opt => opt
+            .BlockAllMixedContent()
+            .StyleSources(s => s.Self().CustomSources("https://fonts.googleapis.com"))
+            .FontSources(s => s.Self().CustomSources("https://fonts.gstatic.com", "data:"))
+            .FormActions(s => s.Self())
+            .FrameAncestors(s => s.Self())
+            .ImageSources(s => s.Self().CustomSources("https://res.cloudinary.com"))
+            .ScriptSources(s => s.Self())
+        );
+
         if (env.IsDevelopment())
         {
             app.UseSwagger();
             app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebAPIv5 v1"));
         }
-
+        else
+        {
+            app.Use(async (context, next) =>
+            {
+                context.Response.Headers.Add("Strict-Transport-Security", "max-age=31536000");
+                await next.Invoke();
+            });
+        }
 
         // app.UseHttpsRedirection();
 
@@ -39,7 +60,7 @@ public class Startup
 
         app.UseDefaultFiles();
         app.UseStaticFiles();
-        
+
 
         app.UseCors("CorsPolicy");
 
